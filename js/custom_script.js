41efae3959ecde7da21ef4463f0e3ce5ddf04a41
Martin Knoll Haakansson <bumstaerk@gmail.com>
2015-11-27 13:52:11 +0100

    final touches to product details

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,1 +1,19 @@
-// Product Details Script
+
+$(document).ready(function(){
+
+  // Product Details Accordion script
+  $('.accordion-navigation').each(function(){
+    var target = $(this);
+    var link = target.find('.accordion-toggle');
+
+    link.on('click', function(e){
+      e.preventDefault();
+      target.toggleClass('active');
+    });
+  });
+
+  // Relocate the onClick function on AddToBasket
+  var onclick = $('.product-basket img').attr('onclick');
+  $('.product-basket-button').attr('onclick', onclick);
+
+});
